5e440206f98555154b086e3851af0da20c434dbb
Davide Carbone <carboneda@gmail.com>
2022-02-14 13:58:28 +0100

    Cleanup unnecessary stuff

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'vendor/autoload.php';
-
-use DI\Container;
-use Slim\Factory\AppFactory;
-
-AppFactory::setContainer(new Container());
-$app = AppFactory::create();
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,3 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
-use DI\Container;
-use Slim\Factory\AppFactory;
-
-AppFactory::setContainer(new Container());
-$app = AppFactory::create();
